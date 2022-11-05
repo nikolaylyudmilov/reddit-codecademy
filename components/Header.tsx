301954f2df93188541f94d8e89e7f476c0ebd8f7
Nikolay <nlyudmilov.bor@gmail.com>
2022-11-05 20:13:03 +0200

    Reddit Login - done

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,8 +16,10 @@ import {
   MenuIcon,
   SearchIcon,
 } from "@heroicons/react/solid";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Header() {
+  const { data: session } = useSession();
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
@@ -60,16 +62,40 @@ function Header() {
         <MenuIcon className="icon" />
       </div>
 
-      <div className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex">
-        <div className="relative h-5 w-5 flex-shrink-0">
-          <Image
-            objectFit="contain"
-            src="/../public/icons/reddit-bl-icon.png"
-            layout="fill"
-          />
+      {session ? (
+        <div
+          onClick={() => signOut()}
+          className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              src="/../public/icons/reddit-bl-icon.png"
+              layout="fill"
+            />
+          </div>
+
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session.user?.name}</p>
+            <p className="text-gray-400">1 Karma</p>
+          </div>
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
         </div>
-        <p className="text-gray-400">Sign In</p>
-      </div>
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              src="/../public/icons/reddit-bl-icon.png"
+              layout="fill"
+            />
+          </div>
+          <p className="text-gray-400">Sign In</p>
+        </div>
+      )}
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
@@ -9,10 +9,8 @@ const Home: NextPage = () => {
       <Head>
         <title>Reddit CodeCademy</title>
       </Head>
-
-      <Header/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
